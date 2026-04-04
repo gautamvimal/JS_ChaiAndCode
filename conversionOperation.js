acac31fa586de86654a35702b015b16c0c1bd9e9
Vimal Gautam <vimal.gautam2302@gmail.com>
2026-04-04 23:25:31 +0530

    conversion

---
+++ conversionOperation.js
@@ -0,0 +1,17 @@
+let score = "50abc";
+console.log(typeof score);
+let conversion = Number(score)
+console.log(typeof conversion);
+console.log(conversion); // NaN (not a number so jab bhi iski value dekhye tho dhyan se dkhye)
+
+
+let isLoggedIn = "vim"
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+
+//1 => true; 0 => flase
+//"" => false
+// "vim" => true 
+
+
---
+++ dataTypes.js
@@ -0,0 +1,15 @@
+"use strict"; //treat all js code as newer version
+//data types
+//number => 2**53 its range
+// bigint
+//string => ""
+// boolean => true/false
+//null=> standalone value
+//undefine => 
+//symbol => for uniqueness (specially use in react)
+
+//object
+
+
+console.log(typeof null); //is object
+console.log(typeof undefined); // but its type is undefined
